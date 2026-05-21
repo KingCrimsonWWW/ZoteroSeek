22b877eaf1ee50c977c5574a29588c6a7a3d4ef0
KingCrimsonWWW <1367908095@qq.com>
2026-05-21 20:44:09 +0800

    fix: handle Cu.unload deprecation and add onMainWindowLoad logging

--- addon/bootstrap.js
+++ addon/bootstrap.js
@@ -31,11 +31,16 @@ async function startup({ id, version, resourceURI, rootURI }, reason) {
 }
 
 async function onMainWindowLoad({ window }, reason) {
-  Zotero.__addonInstance__?.hooks.onMainWindowLoad(window)
+  Zotero.log('[ZoteroSeek] bootstrap onMainWindowLoad called')
+  if (Zotero.__addonInstance__?.hooks?.onMainWindowLoad) {
+    Zotero.__addonInstance__.hooks.onMainWindowLoad(window)
+  }
 }
 
 async function onMainWindowUnload({ window }, reason) {
-  Zotero.__addonInstance__?.hooks.onMainWindowUnload(window)
+  if (Zotero.__addonInstance__?.hooks?.onMainWindowUnload) {
+    Zotero.__addonInstance__.hooks.onMainWindowUnload(window)
+  }
 }
 
 function shutdown({ id, version, resourceURI, rootURI }, reason) {
@@ -49,11 +54,22 @@ function shutdown({ id, version, resourceURI, rootURI }, reason) {
   }
   Zotero.__addonInstance__?.hooks.onShutdown()
 
-  Cc["@mozilla.org/intl/stringbundle;1"]
-    .getService(Components.interfaces.nsIStringBundleService)
-    .flushBundles()
+  try {
+    Cc["@mozilla.org/intl/stringbundle;1"]
+      .getService(Components.interfaces.nsIStringBundleService)
+      .flushBundles()
+  } catch (e) {
+    // Ignore errors during shutdown
+  }
 
-  Cu.unload(`${rootURI}/chrome/content/scripts/__addonRef__.js`)
+  // Cu.unload is deprecated in Zotero 9
+  try {
+    if (typeof Cu.unload === 'function') {
+      Cu.unload(`${rootURI}/chrome/content/scripts/__addonRef__.js`)
+    }
+  } catch (e) {
+    // Ignore unload errors
+  }
 
   if (chromeHandle) {
     chromeHandle.destruct()
